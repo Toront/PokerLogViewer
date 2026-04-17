879d66e8eb17d536b9d9019a17b35611248376bb
Sergei Frolov <Toront1995@gmail.com>
2026-04-17 12:31:09 +0300

    feat: add base MVVM infrastructure

---
+++ src/PokerLogViewer/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PokerLogViewer.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private string _statusText = "Ready";
+
+    public string StatusText
+    {
+        get => _statusText;
+        set => SetProperty(ref _statusText, value);
+    }
+}
